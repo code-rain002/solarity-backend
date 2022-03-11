1e70ccb89c801af0426f2bb562b2cadc474588d7
code-rain002 <jonny93229@gmail.com>
2022-03-11 17:17:33 +0500

    DONE

--- src/modules/User/model.js
+++ src/modules/User/model.js
@@ -5,10 +5,20 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    twitterUsername: { type: String, required: false, unique: true },
-    twitterId: { type: Number, required: false, unique: true },
+    twitterUsername: {
+      type: String,
+      required: false,
+      unique: true,
+      sparse: true,
+    },
+    twitterId: { type: Number, required: false, unique: true, sparse: true },
     password: { type: String, required: true },
-    publicAddress: { type: String, required: false, unique: true },
+    publicAddress: {
+      type: String,
+      required: false,
+      unique: true,
+      sparse: true,
+    },
     coinWatchlist: { type: Array, required: false },
     nftWatchlist: { type: Array, required: false },
     lastNftAnalysis: { type: Date, required: false },

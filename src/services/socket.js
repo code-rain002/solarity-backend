d2f287dd4de7da3813b2e4c37547acd6519dc05e
code-rain002 <jonny93229@gmail.com>
2022-04-08 03:15:25 +0200

    added money-boy-hub.

--- src/services/socket.js
+++ src/services/socket.js
@@ -46,7 +46,6 @@ export const socketService = (io) => {
             curRoom = 'room' + roomId;
             if (!rooms[curRoom]) {
                 rooms[curRoom] = {
-                    name: roomId,
                     occupants: {},
                 };
             }

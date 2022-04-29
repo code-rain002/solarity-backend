706c02f55ab22495c47b9c9d30ddccafb3e4149f
code-rain002 <jonny93229@gmail.com>
2022-04-29 04:10:55 +0200

    changed error message in User/controller.js

--- src/modules/User/controller.js
+++ src/modules/User/controller.js
@@ -93,7 +93,7 @@ export const getUserWithWalletAddressController = async (req, res) => {
       },
       { $unset: Object.keys(USER_DATA_UNSET) },
     ]);
-    if (user.length == 0) throwError("No user with the username exists");
+    if (user.length == 0) throwError("No user with the current wallet exists");
     return successResponse({ res, response: { user: user[0] } });
   } catch (err) {
     return errorResponse({ res, err });

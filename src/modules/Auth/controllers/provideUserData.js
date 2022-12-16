5197337dfdce87ac781cc6c1f6bcd7a7ac85baed
code-rain002 <jonny93229@gmail.com>
2022-12-15 20:12:22 -0800

    changed

--- src/modules/Auth/controllers/provideUserData.js
+++ src/modules/Auth/controllers/provideUserData.js
@@ -3,20 +3,21 @@ import UserModel from "../../User/model";
 
 export const provideUserDataController = async (req, res) => {
   try {
+    return successResponse({ res, response: { profile: "123123" } });
     const { userId } = req.session;
-    let profile = await getProfileData(userId);
-    if(!!profile) {
-      const { visible, stepsCompleted } = profile;
-      if (!visible) {
-        const values = Object.values(stepsCompleted);
-        if (!values.includes(false)) {
-          await UserModel.updateOne({ _id: userId }, { visible: true });
-          profile = await getProfileData(userId);
-        }
-      }
-    } else {
-      profile = {};
-    }
+    // let profile = await getProfileData(userId);
+    // if(!!profile) {
+    //   const { visible, stepsCompleted } = profile;
+    //   if (!visible) {
+    //     const values = Object.values(stepsCompleted);
+    //     if (!values.includes(false)) {
+    //       await UserModel.updateOne({ _id: userId }, { visible: true });
+    //       profile = await getProfileData(userId);
+    //     }
+    //   }
+    // } else {
+    //   profile = {};
+    // }
     return successResponse({ res, response: { profile } });
   } catch (err) {
     return errorResponse({ res, err });
